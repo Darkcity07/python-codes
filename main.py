ccf153744bda1175bc3792b3efc67d0be7ed8182
Darkcity07 <66581982+Darkcity07@users.noreply.github.com>
2023-07-18 14:20:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+a = 5
+b = 8
+
+print("value of a is ", a)
+print("value of b is ", b)
+
+print(a, '+', b, '=', a+b)
+print("a-b = ", a-b)
+print("a*b = ", a*b)
+print("a/b = ", a/b)
+print("a%b = ", a%b)
